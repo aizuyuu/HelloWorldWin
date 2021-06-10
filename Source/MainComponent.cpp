758c5a52ed88e68ad77038c7a38e73957dce9dd0
Muhammad yusreza <aisu.yuusu@gmail.com>
2021-06-10 22:13:08 +0700

    kasi toggle

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -3,6 +3,7 @@
 //==============================================================================
 MainComponent::MainComponent()
 {
+    addAndMakeVisible(tglbtn);
     setSize (600, 400);
 }
 
@@ -26,4 +27,5 @@ void MainComponent::resized()
     // This is called when the MainComponent is resized.
     // If you add any child components, this is where you should
     // update their positions.
+    tglbtn.setBounds(120, 150, 100, 100);
 }
